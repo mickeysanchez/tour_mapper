622e324903af7b5c261f9780fe3266d21ad83d3b
Mickey <sanchez.mickey@gmail.com>
2014-03-14 01:38:26 -0400

    https for github

--- mapper.js
+++ mapper.js
@@ -3,7 +3,7 @@ function ShowsCtrl($scope, $http) {
 	$scope.shows = [];
  
 	$scope.grabShows = function() {
-		$http({method: "GET", url: "http://api.seatgeek.com/2/events?type=concert&q=" + $scope.bandName})
+		$http({method: "GET", url: "https://api.seatgeek.com/2/events?type=concert&q=" + $scope.bandName})
 			.success(function (data) {
 				$scope.shows = data["events"];
 		})
