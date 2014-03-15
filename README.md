bbe351f58a1b403d9776554c15227fcdb72a2c82
Mickey <sanchez.mickey@gmail.com>
2014-03-15 19:27:19 -0400

    refactors out to factories

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 ##[Live](https://rawgithub.com/mickeysanchez/tour_mapper/master/index.html)
 
 Towards a single page, non-sign-in, Angular.js, version of Tour Planner's embeddable tour maps. 
+
+Start a server:
+	from root:
+	option 1: ruby -run -e httpd . -p 8000
+	option 2: python -m SimpleHTTPServer
+	
+	in browser navigate to:
+	http://localhost:8000/app
+
+To edit Sass:
+	from root: 
+	sass --watch css/app.scss:css/app.css
+
--- app/app.js
+++ app/app.js
@@ -1,30 +1,27 @@
-var tourMapper = angular.module('tourMapper', []);
+// create main namespace a.k.a. 'angular module'
+var tourMapper = angular.module('tourMapper', ['ngRoute']);
 
-tourMapper.controller('ShowsCtrl', function($scope, $http, $filter) {
+// config routes (we only have one)
+tourMapper.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', 
+			{
+				controller: 'ShowsCtrl', 
+				templateUrl: 'partials/all.html'
+			})
+		.otherwise({ redirectTo: '/'});	
+});
 
-	$scope.shows = [];
- 
- 	// grab shows from SeatGeek, populate shows and map.
-	$scope.grabShows = function() {
-		$http({method: "GET", url: "https://api.seatgeek.com/2/events?type=concert&q=" + $scope.bandName})
-			.success(function (data) {
-				// clear out any exisitng shows
-				var confirmed = true;
-				
-				if ($scope.shows.length > 0) {
-					var confirmed = confirm("This will replace the shows you alread have. Is that OK?");
-				};
-				
-				if (confirmed) {
-					$scope.shows = [];
-					$scope.shows = $scope.showObjectsFromGrab(data);
-					$scope.addMapMarkers();
-				};
-		})
+// factory
+tourMapper.factory('showFactory', function($http, $filter) {
+	var factory = {};
+	
+	factory.getShowsFromSeatGeek = function (bandName) {
+		return $http({ method: "GET", url: "https://api.seatgeek.com/2/events?type=concert&q=" + bandName });
 	};
 	
-	// turn JSON from SeatGeek API into show objects.
-	$scope.showObjectsFromGrab = function (data) {
+	// turn JSON from SeatGeek API into properly formatted show objects.
+	factory.showObjectsFromGrab = function (data) {
 		return data["events"].map(function (show) {	
 			return  {
 						"ticket_url" : show["url"],
@@ -51,10 +48,48 @@ tourMapper.controller('ShowsCtrl', function($scope, $http, $filter) {
 		});
 	};
 	
+	return factory;
+});
+
+// create a Controller named ShowsCtrl
+tourMapper.controller('ShowsCtrl', function($scope, $http, $filter, showFactory) {
+
+	$scope.shows = [];
+	
+	// create Mapbox Map Object and load an empty map on page load
+	$scope.map = function() {
+		var map = L.mapbox.map('map', 'examples.map-9ijuk24y').setView([30, 10], 2);
+		map.scrollWheelZoom.disable();
+		return map;	 
+	}();
+			   		  	 
+	// instantiate MapBox featureLayer (markers)   
+	$scope.featureLayer = L.mapbox.featureLayer()
+
+ 	// grab shows from SeatGeek, populate shows and map.
+	$scope.grabShows = function() {
+		$scope.shows = showFactory.getShowsFromSeatGeek($scope.bandName)
+		.success(function (data) {
+			var confirmed = true;
+			
+			if ($scope.shows.length > 0) {
+				var confirmed = confirm("This will replace the shows you already have. Is that OK?");
+			};
+			
+			if (confirmed) {
+				// clear out any existing shows
+				$scope.shows = []; 	
+				$scope.shows = showFactory.showObjectsFromGrab(data);
+			};
+		
+			$scope.addMapMarkers();
+		});
+	};
+	
 	$scope.addMapMarkers = function() {
-	    tourMapper.featureLayer.setGeoJSON($scope.shows)
-		tourMapper.map.fitBounds(tourMapper.featureLayer.getBounds());
-		tourMapper.featureLayer.addTo(tourMapper.map);
+	    $scope.featureLayer.setGeoJSON($scope.shows)
+		$scope.map.fitBounds($scope.featureLayer.getBounds());
+		$scope.featureLayer.addTo($scope.map);
 	};
 	
 	$scope.addShow = function(show) {
@@ -63,7 +98,11 @@ tourMapper.controller('ShowsCtrl', function($scope, $http, $filter) {
 	};
 	
 	$scope.showObjectFromForm = function(show) {
-		$http({method: "GET", url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + show.address +  "," + show.city + "," + show.state + "&sensor=false" })
+		$http({
+			method: "GET", 
+			url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + 
+			show.address +  "," + show.city + "," + show.state + "&sensor=false" 
+		})
 		.success(function (data) {
 			console.log(data.results[0]["geometry"]["location"])
 			
@@ -96,17 +135,9 @@ tourMapper.controller('ShowsCtrl', function($scope, $http, $filter) {
 	};
 	
 	$scope.getEmbed = function () {
-		console.log("embed");
 		$body = angular.element( document.querySelector( 'body' ) );
 		$body.prepend( JSON.stringify($scope.shows) );
 	};
  
 });
 
-// load an empty map on page load
-tourMapper.map = L.mapbox.map('map', 'examples.map-9ijuk24y')
-    	   .setView([30, 10], 2);
-		   
-tourMapper.featureLayer = L.mapbox.featureLayer()
-
-tourMapper.map.scrollWheelZoom.disable();
