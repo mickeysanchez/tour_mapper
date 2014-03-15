21ff9d55aceb5bd7c7337bcb285d710a3d68cba0
Mickey <sanchez.mickey@gmail.com>
2014-03-15 19:57:31 -0400

    refactors to different files

---
+++ app/js/controllers/shows_controller.js
@@ -0,0 +1,64 @@
+tourMapper.controller('ShowsCtrl', function($scope, $http, $filter, showFactory) {
+
+	$scope.shows = [];
+	
+	// create Mapbox Map Object and load an empty map on page load
+	$scope.map = function() {
+		var map = L.mapbox.map('map', 'examples.map-9ijuk24y').setView([30, 10], 2);
+		map.scrollWheelZoom.disable();
+		
+		// instantiate MapBox featureLayer (markers)   
+		$scope.featureLayer = L.mapbox.featureLayer()
+		
+		return map;	 
+	}();
+			   		  	 
+ 	// grab shows from SeatGeek, populate shows and map.
+	$scope.grabShows = function() {
+		showFactory.getShowsFromSeatGeek($scope.bandName)
+		.success(function (data) {
+			var confirmed = true;
+			
+			if ($scope.shows.length > 0) {
+				var confirmed = confirm("This will replace the shows you already have. Is that OK?");
+			};
+			
+			if (confirmed) {
+				// clear out any existing shows
+				$scope.shows = []; 	
+				$scope.shows = showFactory.showObjectsFromGrab(data);
+			};
+		
+			$scope.addMapMarkers();
+		});
+	};
+	
+	// creates and places markers for all shows in $scope.shows
+	$scope.addMapMarkers = function() {
+	    $scope.featureLayer.setGeoJSON($scope.shows)
+		$scope.map.fitBounds($scope.featureLayer.getBounds());
+		$scope.featureLayer.addTo($scope.map);
+	};
+	
+	// called by show form
+	$scope.addShow = function(show) {
+		$scope.createShowFromForm(show);
+	};
+	
+	// creates proper show object from form data
+	$scope.createShowFromForm = function(show) {
+		showFactory.createShowFromForm(show)
+		.success(function (data) {
+			var showObj = showFactory.showObjFromFormData(show, data);
+			
+			$scope.shows.push(angular.copy(showObj));
+			$scope.addMapMarkers();
+		});
+	};
+	
+	$scope.getEmbed = function () {
+		$body = angular.element( document.querySelector( 'body' ) );
+		$body.prepend( JSON.stringify($scope.shows) );
+	};
+ 
+});
---
+++ app/js/factories/show_factory.js
@@ -0,0 +1,75 @@
+// factory for grabbing, creating, and formatting show data
+tourMapper.factory('showFactory', function($http, $filter) {
+		var factory = {};
+	
+	// ajax call to SeatGeek API
+	factory.getShowsFromSeatGeek = function (bandName) {
+			return $http({ method: "GET", url: "https://api.seatgeek.com/2/events?type=concert&q=" + bandName });
+	};
+	
+	// turn JSON from SeatGeek API into properly formatted show objects.
+	factory.showObjectsFromGrab = function (data) {
+			return data["events"].map(function (show) {	
+				return  {
+							"ticket_url" : show["url"],
+						"datetime_local": new Date (show["datetime_local"]),
+						"venue": {
+								"name": show["venue"]["name"],
+							"display_location": show["venue"]["display_location"]
+						},
+				        "type": "Feature",
+				        "geometry": {
+					            "type": "Point",
+				            "coordinates": [
+				                show["venue"]["location"]["lon"],
+				                show["venue"]["location"]["lat"]
+				            ]
+				        },
+				        "properties": {
+					            "title": $filter('date')(show["datetime_local"]),
+				            "description": "<a href='" + show["url"] + "'> Tickets </a>",
+				            "marker-size": "small",
+				            "marker-color": "#070"
+				        }
+					};
+		});
+	};
+	
+	// ajax call to Google Geocoding API for show's lat and lon
+	factory.createShowFromForm = function (show) {
+			return $http({
+				method: "GET", 
+			url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + 
+			show.address +  "," + show.city + "," + show.state + "&sensor=false" 
+		})
+	};
+	
+	// returns proper show object from form data and data from
+	// ajax call to Google Geocoding API
+	factory.showObjFromFormData = function (show, data) {
+			return {
+				"ticket_url": show.ticket_url,
+			"datetime_local": new Date (show.datetime_local),
+			"venue": {
+					"name": show.venue.name,
+				"display_location": show.city + ", " + show.state 
+			},
+	        "type": "Feature",
+	        "geometry": {
+		            "type": "Point",
+	            "coordinates": [
+	                data.results[0]["geometry"]["location"]["lng"],
+	                data.results[0]["geometry"]["location"]["lat"]
+	            ]
+	        },
+	        "properties": {
+		            "title": $filter('date')(show.datetime_local),
+	            "description": "<a href='" + show.ticket_url + "'> Tickets </a>",
+	            "marker-size": "small",
+	            "marker-color": "#070"
+	        }
+		};
+	};
+	
+	return factory;
+});
