894b947054f9518d0da8b10c7bc750670f368404
mickeysanchez <sanchez.mickey@gmail.com>
2016-10-11 16:51:28 -0700

    update it to work with new mapbox

--- app/js/controllers/shows_controller.js
+++ app/js/controllers/shows_controller.js
@@ -2,87 +2,88 @@ tourMapper.controller('ShowsCtrl', function($scope, $http, $filter, showFactory,
 
 	// load shows stored in cookies or set shows to empty array
 	$scope.shows = function() {
-		return ( $cookieStore.get("shows") || [] );
+		return ($cookieStore.get("shows") || []);
 	}();
-	
+
 	// create Mapbox Map Object and load an empty map on page load
-	$scope.map = function () {
-		var map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([30, 10], 1);
+	$scope.map = function() {
+		L.mapbox.accessToken = 'pk.eyJ1IjoibWlja2V5c2FuY2hleiIsImEiOiJjaXU2NHl5ZnIwajliMnVxdG9semp1Y2psIn0.C23fYL2agh5b4TqwHIpWng';
+		var map = L.mapbox.map('map', 'mapbox.streets').setView([30, 10], 1);
 		map.scrollWheelZoom.disable();
 		return map;
 	}();
-	
+
 	// MapBox featureLayer (markers) - add shows if shows are in cookies
 	$scope.featureLayer = function() {
 		var featureLayer = $scope.map.featureLayer;
-		
+
 		if ($scope.shows.length > 0) {
 			featureLayer.setGeoJSON($scope.shows);
-			setTimeout(function () {
+			setTimeout(function() {
 				$scope.map.fitBounds(featureLayer.getBounds());
 			}, 200);
 		};
 
 		return featureLayer;
 	}();
-	   		  	 
- 	// grab shows from SeatGeek, populate shows and map.
+
+	// grab shows from SeatGeek, populate shows and map.
 	$scope.grabShows = function() {
 		showFactory.getShowsFromSeatGeek($scope.bandName)
-		.success(function (data) {
-			var confirmed = true;
-			
-			if ($scope.shows.length > 0) {
-				var confirmed = confirm("This will replace the shows you already have. Is that OK?");
-			};
-			
-			if (confirmed) {
-				$scope.shows = showFactory.showObjectsFromGrab(data);
-			};
-		
-			$scope.addMapMarkers();
-			$scope.updateShowsCookie();
-		});
+			.success(function(data) {
+				var confirmed = true;
+
+				if ($scope.shows.length > 0) {
+					var confirmed = confirm("This will replace the shows you already have. Is that OK?");
+				};
+
+				if (confirmed) {
+					$scope.shows = showFactory.showObjectsFromGrab(data);
+				};
+
+				$scope.addMapMarkers();
+				$scope.updateShowsCookie();
+			});
 	};
-	
+
 	// creates and places markers for all shows in $scope.shows
 	$scope.addMapMarkers = function() {
 		$scope.featureLayer.setGeoJSON($scope.shows)
-		
+
 		if ($scope.shows.length === 0) {
 			$scope.map.setView([30, 10], 1);
 		} else {
-			$scope.map.fitBounds($scope.featureLayer.getBounds());		
+			$scope.map.fitBounds($scope.featureLayer.getBounds());
 		};
-		
+
 		$scope.featureLayer.addTo($scope.map);
 	};
-	
+
 	// called by show form
 	$scope.addShow = function(show) {
 		$scope.createShowFromForm(show);
 	};
-	
+
 	// creates proper show object from form data
 	$scope.createShowFromForm = function(show) {
 		showFactory.createShowFromForm(show)
-		.success(function (data) {
-			var showObj = showFactory.showObjFromFormData(show, data);
-			
-			$scope.shows.push(angular.copy(showObj));
-			$scope.addMapMarkers();
-			
-			$scope.show = null;
-			$scope.updateShowsCookie();
-		});
+			.success(function(data) {
+				var showObj = showFactory.showObjFromFormData(show, data);
+
+				$scope.shows.push(angular.copy(showObj));
+				$scope.addMapMarkers();
+
+				$scope.show = null;
+				$scope.updateShowsCookie();
+			});
 	};
-	
+
 	// generate embed code for $scope.shows
-	$scope.getEmbed = function () {
+	$scope.getEmbed = function() {
 		$("textarea").html(showFactory.generateEmbed($scope.shows, $scope.mapWidth, $scope.mapHeight));
-		$("body").addClass("has-active-modal");			
+		$("body").addClass("has-active-modal");
 	};
-	
+
 	// Dynamically update height and width in embed code.
 	$scope.$watch('mapHeight', function() {
 		$("textarea").html(showFactory.generateEmbed($scope.shows, $scope.mapWidth, $scope.mapHeight));
@@ -90,21 +91,21 @@ tourMapper.controller('ShowsCtrl', function($scope, $http, $filter, showFactory,
 	$scope.$watch('mapWidth', function() {
 		$("textarea").html(showFactory.generateEmbed($scope.shows, $scope.mapWidth, $scope.mapHeight));
 	});
-	
+
 	// hide modal overlay when X is clicked
-	$scope.closeModal = function () {
+	$scope.closeModal = function() {
 		$("body").removeClass("has-active-modal");
 	};
-	
+
 	// remove show from $scope.shows, update map and cookie
 	$scope.removeShow = function(idx) {
-	      $scope.shows.splice(idx, 1);
-		  $scope.addMapMarkers();
-		  $scope.updateShowsCookie();
+		$scope.shows.splice(idx, 1);
+		$scope.addMapMarkers();
+		$scope.updateShowsCookie();
 	};
-	
+
 	$scope.updateShowsCookie = function() {
 		$cookieStore.put("shows", $scope.shows)
 	};
-	
+
 });
--- app/js/factories/show_factory.js
+++ app/js/factories/show_factory.js
@@ -1,83 +1,83 @@
 // factory for grabbing, creating, and formatting show data
 tourMapper.factory('showFactory', function($http, $filter) {
-	
+
 	var factory = {};
-	
+
 	// ajax call to SeatGeek API
-	factory.getShowsFromSeatGeek = function (bandName) {
-			return $http({ 
-				method: "GET", 
-				url: "https://api.seatgeek.com/2/events?q=" + bandName 
-			});
-	};
-	
-	// turn JSON from SeatGeek API into properly formatted show objects.
-	factory.showObjectsFromGrab = function (data) {
-			return data["events"].map(function (show) {	
-				return  {
-						"ticket_url" : show["url"],
-						"datetime_local": new Date (show["datetime_local"]),
-						"venue": {
-							"name": show["venue"]["name"],
-							"display_location": show["venue"]["display_location"]
-						},
-				        "type": "Feature",
-				        "geometry": {
-					        "type": "Point",
-				            "coordinates": [
-				                show["venue"]["location"]["lon"],
-				                show["venue"]["location"]["lat"]
-				            ]
-				        },
-				        "properties": {
-					        "title": $filter('date')(show["datetime_local"]),
-				            "description": show["venue"]["display_location"] + "<br>" + "<a href='" + show["url"] + "' target='_blank'> Tickets </a>",
-				            "marker-size": "small",
-				            "marker-color": "#070"
-				        }
-					};
+	factory.getShowsFromSeatGeek = function(bandName) {
+		return $http({
+			method: "GET",
+			url: "https://api.seatgeek.com/2/events?q=" + bandName
 		});
 	};
-	
-	// ajax call to Google Geocoding API for show's lat and lon
-	factory.createShowFromForm = function (show) {
-			return $http({
-				method: "GET", 
-				url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + 
-				show.address +  "," + show.city + "," + show.state + "&sensor=false" 
-			});
+
+	// turn JSON from SeatGeek API into properly formatted show objects.
+	factory.showObjectsFromGrab = function(data) {
+		return data["events"].map(function(show) {
+			return {
+				"ticket_url": show["url"],
+				"datetime_local": new Date(show["datetime_local"]),
+				"venue": {
+					"name": show["venue"]["name"],
+					"display_location": show["venue"]["display_location"]
+				},
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						show["venue"]["location"]["lon"],
+						show["venue"]["location"]["lat"]
+					]
+				},
+				"properties": {
+					"title": $filter('date')(show["datetime_local"]),
+					"description": show["venue"]["display_location"] + "<br>" + "<a href='" + show["url"] + "' target='_blank'> Tickets </a>",
+					"marker-size": "small",
+					"marker-color": "#070"
+				}
+			};
+		});
 	};
-	
+
+	// ajax call to Google Geocoding API for show's lat and lon
+	factory.createShowFromForm = function(show) {
+		return $http({
+			method: "GET",
+			url: "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+				show.address + "," + show.city + "," + show.state + "&sensor=false"
+		});
+	};
+
 	// returns proper show object from form data and data from
 	// ajax call to Google Geocoding API
-	factory.showObjFromFormData = function (show, data) {
-			return {
+	factory.showObjFromFormData = function(show, data) {
+		return {
 			"ticket_url": show.ticket_url,
-			"datetime_local": new Date (show.datetime_local),
+			"datetime_local": new Date(show.datetime_local),
 			"venue": {
 				"name": show.venue.name,
 				"display_location": show.city + ", " + show.state
 			},
-	        "type": "Feature",
-	        "geometry": {
-		        "type": "Point",
-	            "coordinates": [
-	                data.results[0]["geometry"]["location"]["lng"],
-	                data.results[0]["geometry"]["location"]["lat"]
-	            ]
-	        },
-	        "properties": {
-		        "title": $filter('date')(show.datetime_local),
-	            "description": show.city.charAt(0).toUpperCase() + show.city.slice(1) + ", " + show.state.toUpperCase() + "<br>" + "<a href='" + show.ticket_url + "' target='_blank'> Tickets </a>",
-	            "marker-size": "small",
-	            "marker-color": "#070"
-	        }
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					data.results[0]["geometry"]["location"]["lng"],
+					data.results[0]["geometry"]["location"]["lat"]
+				]
+			},
+			"properties": {
+				"title": $filter('date')(show.datetime_local),
+				"description": show.city.charAt(0).toUpperCase() + show.city.slice(1) + ", " + show.state.toUpperCase() + "<br>" + "<a href='" + show.ticket_url + "' target='_blank'> Tickets </a>",
+				"marker-size": "small",
+				"marker-color": "#070"
+			}
 		};
 	};
-	
-	factory.generateEmbed = function (shows, width, height) {
-		return "&lt;!-- Map Styles --&gt; \n&lt;style&gt; #map { border: 1px solid black; height: " + height + "px; width: " + width + "px; } &lt;/style&gt; \n\n&lt;!-- This is where the map is drawn. --&gt; \n&lt;div id='map'&gt;&lt;/div&gt; \n\n&lt;!-- Required libraries. Props to Mapbox --&gt; \n&lt;script src='https://api.tiles.mapbox.com/mapbox.js/v1.6.1/mapbox.js'&gt;&lt;/script&gt; &lt;link href='https://api.tiles.mapbox.com/mapbox.js/v1.6.1/mapbox.css' rel='stylesheet' /&gt; \n\n&lt;!-- Draws markers. Contains show data. --&gt; \n&lt;script&gt; var map = L.mapbox.map('map', 'examples.map-9ijuk24y').setView([30, 10], 2); map.scrollWheelZoom.disable(); var featureLayer = L.mapbox.featureLayer().setGeoJSON(" + JSON.stringify(shows) + "); map.fitBounds(featureLayer.getBounds()); featureLayer.addTo(map); &lt;/script&gt;"
+
+	factory.generateEmbed = function(shows, width, height) {
+		return "&lt;!-- Map Styles --&gt; \n&lt;style&gt; #map { border: 1px solid black; height: " + height + "px; width: " + width + "px; } &lt;/style&gt; \n\n&lt;!-- This is where the map is drawn. --&gt; \n&lt;div id='map'&gt;&lt;/div&gt; \n\n&lt;!-- Required libraries. Props to Mapbox --&gt; \n&lt;script src='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js'&gt;&lt;/script&gt; &lt;link href='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.css' rel='stylesheet' /&gt; \n\n&lt;!-- Draws markers. Contains show data. --&gt; \n&lt;script&gt; var map = L.mapbox.map('map', 'examples.map-9ijuk24y').setView([30, 10], 2); map.scrollWheelZoom.disable(); var featureLayer = L.mapbox.featureLayer().setGeoJSON(" + JSON.stringify(shows) + "); map.fitBounds(featureLayer.getBounds()); featureLayer.addTo(map); &lt;/script&gt;"
 	};
-	
+
 	return factory;
 });
